b9a89d7c4011d254a125b2c39bda012b17bf13f1
lemon橪 <17633066053@163.com>
2022-02-14 14:31:29 +0800

    新增分销模块分销业绩详细信息补充

--- pages/mine/distribution/history.vue
+++ pages/mine/distribution/history.vue
@@ -1,7 +1,12 @@
 <template>
   <view class="log-list">
     <!-- 提现记录 -->
-    <view class="log-way" v-if="cashLogData.length != 0" v-for="(item, index) in cashLogData" :key="index">
+    <view
+      class="log-way"
+      v-if="cashLogData.length != 0"
+      v-for="(item, index) in cashLogData"
+      :key="index"
+    >
       <view class="log-item">
         <view class="log-item-view">
           <view class="title">{{
@@ -20,21 +25,32 @@
       </view>
     </view>
     <!-- 分销业绩 -->
-    <view class="log-way" v-if="achievementData.length != 0" v-for="(item, index) in achievementData" :key="index">
+    <view
+      class="log-way"
+      v-if="achievementData.length != 0"
+      v-for="(item, index) in achievementData"
+      :key="index"
+    >
       <view class="log-item">
         <view class="log-item-view">
-          <view class="title">{{item.goodsName}}</view>
-          <view class="price">+{{ item.rebate | unitPrice }}</view>
+          <view class="title">{{ item.goodsName }}</view>
+          <view class="price">提成金额：+{{ item.rebate | unitPrice }}</view>
         </view>
         <view class="log-item-view">
-          <view>{{ item.createTime }}</view>
-          <view>{{item.storeName}}</view>
+          <view>创建时间：{{ item.createTime }}</view>
+          <view>店铺：{{ item.storeName }}</view>
+        </view>
+        <view class="log-item-footer">
+          <view>会员名称：{{ item.memberName }}</view>
+          <view>订单金额：{{ item.flowPrice | unitPrice }}</view>
+        </view>
+        <view class="log-item-footers">
+          <view>订单号：{{ item.orderSn }}</view>
         </view>
       </view>
     </view>
     <view class="empty" v-if="empty">
       <u-loadmore :status="status" :icon-type="iconType" bg-color="#f7f7f7" />
-
     </view>
   </view>
 </template>
@@ -86,7 +102,7 @@ export default {
       });
       distributionOrderList(this.achParams).then((res) => {
         if (res.data.success && res.data.result.records.length >= 1) {
-          this.achievementData = res.data.result.records;
+          this.achievementData.push(...res.data.result.records);
         } else {
           this.status = "nomore";
           this.empty = true;
@@ -101,7 +117,7 @@ export default {
       });
       cashLog(this.params).then((res) => {
         if (res.data.success && res.data.result.records.length >= 1) {
-          this.cashLogData = res.data.result.records;
+          this.cashLogData.push(...res.data.result.records);
         } else {
           this.status = "nomore";
           this.empty = true;
@@ -139,6 +155,19 @@ export default {
 .log-item-view {
   padding: 8rpx 32rpx;
   display: flex;
+  font-size: 13px;
+  justify-content: space-between;
+}
+.log-item-footer {
+  padding: 8rpx 32rpx;
+  display: flex;
+  font-size: 13px;
+  justify-content: space-between;
+}
+.log-item-footers {
+  padding: 8rpx 32rpx;
+  display: flex;
+  font-size: 13px;
   justify-content: space-between;
 }
 </style>
