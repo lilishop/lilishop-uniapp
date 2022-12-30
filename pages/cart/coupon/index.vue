f84f9ab696c10447c7aa8a27382cfccd3d5729d8
yeliang-king <2244255345@qq.com>
2022-12-30 17:33:05 +0800

    优惠券种类字段修改

--- pages/cart/coupon/index.vue
+++ pages/cart/coupon/index.vue
@@ -35,7 +35,7 @@
         <view class="desc">
           <view v-if="item.scopeType">
             <span v-if="item.scopeType == 'ALL' && item.storeId == '0'">全平台</span>
-            <span v-if="item.scopeType == 'PORTION_CATEGORY'">仅限品类</span>
+            <span v-if="item.scopeType == 'PORTION_GOODS_CATEGORY'">仅限品类</span>
             <view v-else
               >{{
                 item.storeName == "platform" ? "全平台" : item.storeName + "店铺"
