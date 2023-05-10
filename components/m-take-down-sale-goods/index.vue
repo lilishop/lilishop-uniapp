ca4e458db1094e351ef02e29ab3c9b5b429fe2eb
Yer <17633066053@163.com>
2023-05-10 10:46:23 +0800

    feat: :sparkles: 新增商品下架提示框，在原有的下架提示中优化商品展示页面，弹出下架提示，展示其他商品功能。

---
+++ components/m-take-down-sale-goods/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <u-popup v-model="show" mode="bottom" height="800rpx" border-radius="14">
+      <div class="wrapper">
+        <view class="down-goods-tips">该商品已下架</view>
+        <scroll-view scroll-y="true" style="height: 670rpx">
+          <goodsRecommend title="其他商品" />
+        </scroll-view>
+      </div>
+    </u-popup>
+  </div>
+</template>
+
+<script>
+import goodsRecommend from "@/components/m-goods-recommend/index.vue";
+
+export default {
+  data() {
+    return {
+      show: true, // 是否显示
+    };
+  },
+  components: { goodsRecommend },
+};
+</script>
+
+<style lang="scss" scoped>
+.down-goods-tips {
+  font-size: 36rpx;
+  text-align: center;
+  font-weight: bold;
+  padding: 40rpx 0;
+}
+</style>
